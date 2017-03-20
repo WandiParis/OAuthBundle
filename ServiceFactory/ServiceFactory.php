455b7676c9b4482f607ab04806c811c70a91da7f
jacquesndl <jacques.delamballerie@gmail.com>
2017-03-20 10:26:01 +0100

    add default token

--- ServiceFactory/ServiceFactory.php
+++ ServiceFactory/ServiceFactory.php
@@ -5,6 +5,8 @@ namespace Wandi\OAuthBundle\ServiceFactory;
 use OAuth\Common\Consumer\Credentials;
 use OAuth\Common\Exception\Exception;
 use OAuth\Common\Storage\TokenStorageInterface;
+use OAuth\OAuth1\Token\StdOAuth1Token;
+use OAuth\OAuth2\Token\StdOAuth2Token;
 use OAuth\ServiceFactory as BaseServiceFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -50,26 +52,32 @@ class ServiceFactory
     public function createService($resourceOwnerName)
     {
         if (!isset(ResourceOwners::$all[$resourceOwnerName])) {
-            throw new Exception('Resource owner ' . $resourceOwnerName . ' is not available');
+            throw new Exception('Resource owner '.$resourceOwnerName.' is not available');
         }
 
         if (isset($this->serviceCache[$resourceOwnerName])) {
             return $this->serviceCache[$resourceOwnerName];
         }
 
-        $lowerResourceOwnerName = $string = strtolower(preg_replace('/(?<=\\w)(?=[A-Z])/',"_$1", $resourceOwnerName));
+        $lowerResourceOwnerName = $string = strtolower(preg_replace('/(?<=\\w)(?=[A-Z])/', "_$1", $resourceOwnerName));
 
         $credentials = new Credentials(
-            $this->container->getParameter('wandi_oauth.resource_owners.' . $lowerResourceOwnerName . '.client_id'),
-            $this->container->getParameter('wandi_oauth.resource_owners.' . $lowerResourceOwnerName . '.client_secret'),
-            $this->container->get('router')->generate($this->container->getParameter('wandi_oauth.resource_owners.' . $lowerResourceOwnerName . '.callback_route'), [], UrlGeneratorInterface::ABSOLUTE_URL)
+            $this->container->getParameter('wandi_oauth.resource_owners.'.$lowerResourceOwnerName.'.client_id'),
+            $this->container->getParameter('wandi_oauth.resource_owners.'.$lowerResourceOwnerName.'.client_secret'),
+            $this->container->get('router')->generate($this->container->getParameter('wandi_oauth.resource_owners.'.$lowerResourceOwnerName.'.callback_route'), [], UrlGeneratorInterface::ABSOLUTE_URL)
         );
 
-        $scopes = $this->container->getParameter('wandi_oauth.resource_owners.' . $lowerResourceOwnerName . '.scopes');
-        $baseApiUrl = $this->container->getParameter('wandi_oauth.resource_owners.' . $lowerResourceOwnerName . '.base_api_url');
-        $apiVersion = $this->container->getParameter('wandi_oauth.resource_owners.' . $lowerResourceOwnerName . '.api_version');
+        $scopes = $this->container->getParameter('wandi_oauth.resource_owners.'.$lowerResourceOwnerName.'.scopes');
+        $baseApiUrl = $this->container->getParameter('wandi_oauth.resource_owners.'.$lowerResourceOwnerName.'.base_api_url');
+        $apiVersion = $this->container->getParameter('wandi_oauth.resource_owners.'.$lowerResourceOwnerName.'.api_version');
 
-        return $this->serviceCache[$resourceOwnerName] = $this->factory->createService($resourceOwnerName, $credentials, $this->storage, $scopes, $baseApiUrl, $apiVersion);
+        $service = $this->factory->createService($resourceOwnerName, $credentials, $this->storage, $scopes, $baseApiUrl, $apiVersion);
+
+        if (!$this->storage->hasAccessToken($resourceOwnerName)) {
+            $this->storage->storeAccessToken($resourceOwnerName, in_array($resourceOwnerName, ResourceOwners::$oauth1) ? new StdOAuth1Token() : new StdOAuth2Token());
+        }
+
+        return $this->serviceCache[$resourceOwnerName] = $service;
     }
 }
  
